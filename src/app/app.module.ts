ce628ac9be5e8ecea57be9536bc0a4397c3c01ee
Sean Veal <seanveal@Seans-MacBook-Pro.local>
2018-07-25 10:37:36 -0500

    feature: created register + home components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpModule} from '@angular/http';
+import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 
 
@@ -8,20 +8,24 @@ import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       ValueComponent,
-      NavComponent
+      NavComponent,
+      HomeComponent,
+      RegisterComponent
    ],
    imports: [
       BrowserModule,
-      HttpModule,
+      HttpClientModule,
       FormsModule
    ],
    providers: [
-       AuthService
+      AuthService
    ],
    bootstrap: [
       AppComponent
--- src/app/value/value.component.ts
+++ src/app/value/value.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-value',
@@ -9,16 +8,8 @@ import { Http } from '@angular/http';
 export class ValueComponent implements OnInit {
   values: any;
 
-  constructor(private http: Http) { }
+  constructor() { }
 
-  ngOnInit() {
-    this.getValues();
-  }
-
-  getValues() {
-    this.http.get('http://localhost:5000/api/values').subscribe(response => {
-      this.values = response.json();
-    });
-  }
+  ngOnInit() {}
 
 }
