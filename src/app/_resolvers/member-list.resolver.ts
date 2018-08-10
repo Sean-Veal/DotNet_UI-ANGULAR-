a993241c8b7b9672a8b050f53feb389acf0216d2
Sean Veal <seanveal@Seans-MacBook-Pro.local>
2018-08-10 08:52:29 -0500

    feature: Photo Gallery implemented

--- src/app/_resolvers/member-list.resolver.ts
+++ src/app/_resolvers/member-list.resolver.ts
@@ -8,7 +8,7 @@ import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class MemberListResolver implements Resolve<User[]> {
-    constructor(private userService: UserService, private router: Router, 
+    constructor(private userService: UserService, private router: Router,
         private alertify: AlertifyService) {}
 
     resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
