2b8757b43bd3b00c97a7e3dfbe1e52e0b317d0d5
Sean Veal <seanveal@Seans-MacBook-Pro.local>
2018-08-13 14:21:44 -0500

    blocker: mapper setting values to null (for some reason)

---
+++ src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { User } from '../../_models/user';
+import { AlertifyService } from '../../_services/alertify.service';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-member-edit',
+  templateUrl: './member-edit.component.html',
+  styleUrls: ['./member-edit.component.css']
+})
+export class MemberEditComponent implements OnInit {
+  user: User;
+  @ViewChild('editForm') editForm: NgForm;
+  @HostListener('window:beforeunload', ['$event'])
+  unloadNotification($event: any) {
+    if (this.editForm.dirty) {
+      $event.returnValue = true;
+    }
+  }
+
+  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
+
+  ngOnInit() {
+    this.route.data.subscribe(data => {
+      this.user = data['user'];
+    });
+  }
+
+  updateUser() {
+    console.log(this.user);
+    this.alertify.success('User updated successfully');
+    this.editForm.reset(this.user);
+  }
+
+}
